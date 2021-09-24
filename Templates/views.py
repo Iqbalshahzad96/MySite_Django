3390e7a9ee68bd1824d3171ae6b4635ef3111f07
iqbalshahzad96 <iqbalshahzad1996@gmail.com>
2021-09-24 16:12:41 +0500

    Templates Filters

--- Templates/views.py
+++ Templates/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'learn_templates/index.html')
+    context_dict = {'text':'hello', 'number':100}
+    return render(request,'learn_templates/index.html', context_dict)
 
 def base(request):
     return render(request,'learn_templates/base.html')
